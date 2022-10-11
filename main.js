fab82e6e907b715667ca8144b59ea080a0a189b4
BermudezSergio <sergio.bermudez@live.u-tad.com>
2022-10-11 13:07:15 +0200

    Version para la nuve

---
+++ main.js
@@ -0,0 +1,63 @@
+
+var participants = [];
+
+
+const getWinner = (data) => {
+
+    const aleat = Math.floor(Math.random() * data.length);
+  
+    $("#selected").html("<b>Ha gando: </b>"+data[aleat].name);
+
+
+}
+
+const showParticipants = (data) => {
+
+    let arrayAux=[];
+
+    data.forEach(element => {
+        arrayAux.push(element.name);
+    });
+
+    //$("#users").text("Participants"+ arrayAux);
+    //$("#users").html("<strong>Participants: </strong> "+arrayAux);
+    $("#users").text(arrayAux);
+
+}
+
+
+$(document).ready(function(){
+
+    $.ajax({
+
+        type: "GET",
+        url: "https://jsonplaceholder.typicode.com/users",
+        contentType: "application/json",
+        success:
+            function(response){
+
+                participants=response;
+
+                showParticipants(participants);
+
+            },
+        error: function(error){
+            console.log(error);
+            alert(error);
+
+        }
+
+    });
+
+
+    $(".btn").click(function () {
+
+
+        getWinner(participants);
+    
+    });
+
+
+});
+
+
